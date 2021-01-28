a227c570435ded20d2bd1503ad096c61c548beac
Josh Seltzer <jselt88@gmail.com>
2021-01-27 20:09:15 -0500

    additional comments

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
 
+//setup main route
 const mainRoutes = require('./routes');
 
+//use main route before routing through errors
 app.use(mainRoutes);
 
+//setup error routing
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
+    const err = new Error('Page Not Found');
     err.status = 404;
     next(err);
 });
 
+//global error handler
 app.use((err, req, res, next)=>{
     res.locals.error = err;
     res.status(err.status);
     res.render('error', err);
 });
 
+//open port
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 })
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,34 @@ const router =express.Router();
 
 const {projects} = require('../data.json');
 
+
+//Home route
 router.get('/', (req, res) => {
     const projectList = projects;
     res.render('index', {projectList})
 });
 
+
+//Route for about page
 router.get('/about', (req, res) => {
     res.render('about')
 });
 
+
+//Route for projects with catch for projects not in database
 router.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
     const project = projects.find(({id}) => id === +projectId);
+    console.log(project);
     const projectN = project;
-    res.render('project', {projectN});
+    if (project) {
+        res.render('project', {projectN});
+    } else {
+        const err = new Error('Project Not Found');
+        err.status = 404;
+        req.next(err);
+    }
+
 });
 
 module.exports = router;
